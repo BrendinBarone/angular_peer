c0a280e07881c8967c07e7045ae0f00a5b906cd6
Brendin Barone <robotcityhq@gmail.com>
2017-07-14 15:27:37 -0500

    base mode complete, delete button added

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,12 +2,24 @@ console.log('js is sourced');
 
 var myApp = angular.module('myApp', []);
 
-myApp.controller('PersonController', function(){
+myApp.controller('PersonController', function() {
   // Code that runs when our controller is initialized
   // Similar to document ready
   var person = this;
   console.log(this);
-  person.message = 'Hello!';
+
   console.log('PersonController');
 
+  person.people = [];
+  person.addPerson = function(name, message) {
+    var newPerson = {
+      name: name,
+      message: message
+    };
+    person.people.push(newPerson);
+    console.log(person.people);
+  };
+  person.delete = function(index) {
+    person.people.splice(index, 1);
+  };
 });
